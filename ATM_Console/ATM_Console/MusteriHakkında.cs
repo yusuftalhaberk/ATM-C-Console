5ef03b022544b46b24ace6feba1f643a221a1329
Yusuf Talha <pctalha42@gmail.com>
2026-05-22 19:36:19 +0300

    Add files via upload

---
+++ ATM_Console/ATM_Console/MusteriHakkında.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ATM_Console
+{
+    internal class MusteriHakkında
+    {
+        public int musteriNo;
+        private string musteriIsmi;
+        private string musteriParola;
+
+        public int bakiye;
+
+        public string MusteriIsmi
+        {
+            get { return musteriIsmi; }
+            set{ musteriIsmi = value; }
+        }
+
+        public void MusteriNoOlustur()
+        {
+            Random rnd = new Random();
+            musteriNo = rnd.Next(0, 101);
+        }
+
+        public string MusteriParola
+        {
+            get { return musteriParola; }
+            set { musteriParola = value; }
+        }
+
+        public void yeniuyebilgi()
+        {
+            Console.WriteLine($"Sayın {musteriIsmi} adlı kullanıcımız müşteri numaranız: {musteriNo}");
+        }
+
+        public void ParaEkle(int tutar)
+        {
+            bool islem = false;
+
+            if (tutar > 0)
+            {
+                bakiye += tutar;
+                islem = true;
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz tutar geçersizdir tekrar deneyiniz");
+            }
+            
+        }
+
+        public void ParaCek(int tutar)
+        {
+            bool islem = false;
+
+            if (tutar > 0 && tutar <= bakiye)
+            {
+                bakiye -= tutar;
+                islem = true;
+            }
+            else if (tutar > 0 && bakiye > 0)
+            {
+                Console.WriteLine("Girdiğiniz tutar bakiye üzerinde lütfen tekrar deneyiniz");
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz tutar geçersizdir tekrar deneyiniz");
+            }
+        }
+
+    }
+}
---
+++ ATM_Console/ATM_Console/Program.cs
@@ -0,0 +1,117 @@
+﻿using ATM_Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MusteriHakkında[] musteriler = new MusteriHakkında[10];//tüm sistem 10 kişi ile sınırlı
+        int i = 0;
+        while (true)
+        {
+            Console.WriteLine("##ATM ye HOŞGELDİNİZ##\nYapmak istediğiniz işlemi tuşlayınız: ");
+            Console.WriteLine("1-Üye Ol");
+            Console.WriteLine("2-Giriş Yap");
+            Console.WriteLine("3-Çık");
+            int birincisecim = Convert.ToInt32(Console.ReadLine());
+            if (birincisecim == 1)
+            {
+                if (i >= musteriler.Length)
+                {
+                    Console.WriteLine("Maksimum müşteri sayısına ulaşıldı!");
+                    continue;
+                }
+                musteriler[i] = new MusteriHakkında();
+                Console.Write("Lütfen isminizi yazın: ");
+                musteriler[i].MusteriIsmi = Console.ReadLine();
+                musteriler[i].MusteriNoOlustur();
+                Console.Write("Lütfen parolanızı yazın: ");
+                musteriler[i].MusteriParola = Console.ReadLine();
+                musteriler[i].yeniuyebilgi();
+                i++;
+
+            }
+            else if (birincisecim == 2)
+            {
+                int j;
+                bool sifredeneme = true;
+                while (true)
+                {
+                    bool numarabulma = false;
+                    Console.Write("Müşteri numaranızı giriniz: ");
+                    int girilennumara = Convert.ToInt32(Console.ReadLine());
+                    for (j = 0; j < 10; j++)
+                    {
+                        if (musteriler[j] != null && musteriler[j].musteriNo == girilennumara)
+                        {
+                            numarabulma = true;
+                            break;
+                        }
+                    }
+                    if (numarabulma)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Böyle bir kullanıcı yok tekrar dene");
+                    }
+
+                }
+                while (sifredeneme)
+                {
+                    Console.Write("Lütfen parolanızı giriniz: ");
+                    string girilenparola = Console.ReadLine();
+                    if (musteriler[j].MusteriParola == girilenparola)
+                    {
+                        sifredeneme = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parola yanlış!!!");
+                    }
+                }
+                while (true)
+                {
+                    Console.WriteLine("Şuanki bakiyeniz: " + musteriler[j].bakiye);
+                    Console.WriteLine("Yapmak istediğiniz işlemi seçiniz");
+                    Console.WriteLine("1-Para ekle");
+                    Console.WriteLine("2-Para çek");
+                    Console.WriteLine("3-Çıkış");
+                    int ikincisecim = Convert.ToInt32(Console.ReadLine());
+                    if (ikincisecim == 1)
+                    {
+                        Console.WriteLine("Eklemek istediğiniz tutarı giriniz:");
+                        int eklenecek = Convert.ToInt32(Console.ReadLine());
+                        musteriler[j].ParaEkle(eklenecek);
+                    }
+                    else if (ikincisecim == 2)
+                    {
+                        Console.WriteLine("Çekmek istediğiniz tutarı giriniz:");
+                        int cekilecek = Convert.ToInt32(Console.ReadLine());
+                        musteriler[j].ParaCek(cekilecek);
+                    }
+                    else if (ikincisecim == 3)
+                    {
+                        Console.WriteLine("Çıkış yapıldı");
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Hatalı tuşlama tekrar dene");
+                    }
+                }
+
+            }
+            else if (birincisecim == 3)
+            {
+                Console.WriteLine("Çıkış yapıldı");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Hatalı tuşlama tekrar dene");
+            }
+        }
+    }
+}
